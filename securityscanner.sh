dc1ed95313e1484172d073ee21239fddea78cf98
Christiaan lips <chris.lips.business@gmail.com>
2020-11-26 14:49:58 +0000

    kubehunter logging back to YAML format for logstash readability

--- securityscanner.sh
+++ securityscanner.sh
@@ -50,7 +50,7 @@ spec:
         command: ["kube-hunter"]
         args:
           - '--report'
-          - json
+          - yaml
           - '--log'
           - none
           - '--remote'" > kubehunterjob.yaml
@@ -83,5 +83,5 @@ rm -rf kubehunterjob.yaml
 echo -e "\e[32m================================="
 echo -e "+ Exiting security application +"
 echo -e "=================================\e[0m"
-echo -e "\e[32mAll logging is stored under the 'Securityscanogs' folder\e[0m"
+echo -e "\e[32mAll logging is stored under the 'Securityscanlogs' folder\e[0m"
  
