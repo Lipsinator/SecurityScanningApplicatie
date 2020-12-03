1c372df2c4bba001cf33f3217506f3fff0d0e661
TheChrisKip <43065686+TheChrisKip@users.noreply.github.com>
2020-12-03 14:55:30 +0100

    Update securityscanner.sh
    3-12-2020 update, read changelog

--- securityscanner.sh
+++ securityscanner.sh
@@ -1,3 +1,5 @@
+
+
 #! /bin/bash
 #==============================================================
 # Security scanner script - Christiaan Lips
@@ -12,14 +14,16 @@
 # 26-11-2020 - ADDED - now gathers quay vulnerability reports more efficiently.
 #            - ADDED - now creates own folders and removes existing jobs and used yaml files.
 # 30-11-2020 - ADDED - now formats reports optimally for logstash.
-#
+# 3-12-2020  - ADDED - now automatically creates route for polaris dashboard.
 #
  
 clear
 echo -e "\e[32m================================="  
 echo -e "+ Starting security application +"
 echo -e "=================================\e[0m"
-echo -e "\e[31mPlease note that in order for this application to work you need to be logged in on your OpenShift platform and have root access\e[0m"
+echo -e "\e[31mPlease note that in order for this application to work you need to be logged in on your OpenShift platform and have root access.\n Please make sure that there is no namespace called 'kubehunter' when executing this script.\e[0m"
+read -p "Press any key to continue: "
+
 mkdir SecurityLogs
 
 #------------------------------------- Quay Security Operator ----------------------------------
@@ -35,9 +39,9 @@ echo "                    {
 
 #------------------------------------- Kube-hunter ---------------------------------------------
 		
-# create and switch to the kunehunter namespace, if there are any old kube-hunter jobs left delete them.
+# create and switch to the kunehunter namespace
 oc create ns kubehunter
-oc project kubehunter && oc delete jobs --all
+oc project kubehunter
 
 # create the job with substituted nodes
 echo "apiVersion: batch/v1
@@ -76,18 +80,20 @@ oc logs `oc get pods | awk '{print $1, $8}' |  sed 1,1d` > SecurityLogs/kubehunt
 sed -i '/- location: /,$!d' SecurityLogs/kubehunterlogs.txt 
 echo "- location: " >> SecurityLogs/kubehunterlogs.txt
 
+# clean up the used job
+rm -rf kubehunterjob.yaml
+
+
 #---------------------------------------------------------------------------------------------
 
 # install polaris to check for kubernetes configuration best practices
 kubectl apply -f https://github.com/FairwindsOps/polaris/releases/latest/download/dashboard.yaml
 
-# clean up the used job
-rm -rf kubehunterjob.yaml
+kubectl create -f polarisroute.yaml
+
                                                  
 echo -e "\e[32m================================="
 echo -e "+ Exiting security application +"
 echo -e "=================================\e[0m"
 echo -e "\e[32mAll logging is stored under the 'SecurityLogs' folder\e[0m"
-
-
  
